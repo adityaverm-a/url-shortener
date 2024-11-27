13094f47e405263e4e854e157de5c4b748fdce3e
Aditya Verma <aditya.se.403@gmail.com>
2024-11-28 02:55:54 +0530

    repo interface added

--- domain/repositories/us_repo.go
+++ domain/repositories/us_repo.go
@@ -1,11 +1,15 @@
 package repositories
 
+import (
+	"url-shortener/domain/entities"
+)
+
 // UrlShortenerRepository is...
 //
 //go:generate mockgen -destination=mocks/mock_us_repo.go -package=mocks url-shortener/domain/repositories UrlShortenerRepository
 type UrlShortenerRepository interface {
-	// GetByID(id int64) (*models.Order, error)
-	// GetByFilters(filters entities.OrderFiltersInput) (*[]models.Order, error)
-	// Create(input entities.CreateOrderInput) (*models.Order, error)
-	// Update(input entities.UpdateOrderStatusInput) (*models.Order, error)
+	Save(input entities.CreateShortURLInput) error
+	GetByLongURL(url string) (*entities.URL, error)
+	GetByShortURL(url string) (*entities.URL, error)
+	IncrementAccessCount(shortURL string) error
 }
