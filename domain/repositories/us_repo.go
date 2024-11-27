b0d783e1ac29e01276f957641eb5029e042f1638
Aditya Verma <aditya.se.403@gmail.com>
2024-11-28 03:55:09 +0530

    service shorten url method implemented

--- domain/repositories/us_repo.go
+++ domain/repositories/us_repo.go
@@ -8,7 +8,7 @@ import (
 //
 //go:generate mockgen -destination=mocks/mock_us_repo.go -package=mocks url-shortener/domain/repositories UrlShortenerRepository
 type UrlShortenerRepository interface {
-	Save(input entities.CreateShortURLInput) error
+	Save(input entities.URL) error
 	GetByLongURL(url string) (*entities.URL, error)
 	GetByShortURL(url string) (*entities.URL, error)
 	IncrementAccessCount(shortURL string) error
--- domain/services/us_service.go
+++ domain/services/us_service.go
@@ -1,13 +1,17 @@
 package services
 
 import (
+	"time"
+	"url-shortener/domain/entities"
 	"url-shortener/domain/repositories"
+
+	"golang.org/x/exp/rand"
 )
 
 // UrlShortenerService is...
 type UrlShortenerService interface {
-	ShortURL(shortURL string) (string, error)
-	ResolveURL(longURL string) (string, error)
+	ShortenURL(longURL string) (string, error)
+	ResolveURL(shortURL string) (string, error)
 }
 
 // The NewUrlShortenerService function is a factory function that returns a new instance of the urlShortenerService
@@ -19,22 +23,43 @@ type urlShortenerService struct {
 	repo repositories.UrlShortenerRepository
 }
 
-// // The GetOrdersByFilters method of the orderService struct utilizes the OrderRepository instance, by calling the GetByFilters method on it, and returns all orders from the given filters and errors if received.
-// func (service *orderService) GetOrdersByFilters(filters entities.OrderFiltersInput) (*[]models.Order, error) {
-// 	return service.repo.GetByFilters(filters)
-// }
+// The GetOrderByID method of the urlShortenerService struct utilizes the OrderRepository instance, by calling the GetByID method on it, and returns any order and errors if received.
+func (service *urlShortenerService) ShortenURL(longURL string) (string, error) {
+	existing, _ := service.repo.GetByLongURL(longURL)
+	if existing != nil {
+		return existing.ShortURL, nil
+	}
 
-// // The GetOrderByID method of the orderService struct utilizes the OrderRepository instance, by calling the GetByID method on it, and returns any order and errors if received.
-// func (service *orderService) GetOrderByID(id int64) (*models.Order, error) {
-// 	return service.repo.GetByID(id)
-// }
+	shortURL := service.generateShortURL(6)
 
-// // The CreateOrder method of the orderService struct utilizes the OrderRepository instance, by calling the Create method on it, and returns the order created and errors if received.
-// func (service *orderService) CreateOrder(input entities.CreateOrderInput) (*models.Order, error) {
+	url := entities.URL{
+		LongURL:  longURL,
+		ShortURL: shortURL,
+	}
+
+	err := service.repo.Save(url)
+
+	return shortURL, err
+}
+
+func (service *urlShortenerService) generateShortURL(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// Seed the random number generator to ensure randomness
+	rand.Seed(uint64(time.Now().UnixNano()))
+
+	// Create a slice of runes to store the random string
+	result := make([]byte, length)
+
+	// Generate the random string
+	for i := range result {
+		result[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(result)
+}
+
+// The CreateOrder method of the urlShortenerService struct utilizes the OrderRepository instance, by calling the Create method on it, and returns the order created and errors if received.
+// func (service *urlShortenerService) ResolveURL(shortURL string) (string, error) {
 // 	return service.repo.Create(input)
-// }
-
-// // The UpdateOrderStatus method of the orderService struct utilizes the OrderRepository instance, by calling the Update method on it, and returns the order with updated status and errors if received.
-// func (service *orderService) UpdateOrderStatus(input entities.UpdateOrderStatusInput) (*models.Order, error) {
-// 	return service.repo.Update(input)
 // }
