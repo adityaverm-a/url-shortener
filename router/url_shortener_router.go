11f43e93a56f0a2609c5dfb71c2beb9f122a5ade
Aditya Verma <aditya.se.403@gmail.com>
2024-11-29 01:16:52 +0530

    refactoring router layer

--- router/url_shortener_router.go
+++ router/url_shortener_router.go
@@ -9,30 +9,36 @@ import (
 
 var urlShortenerController controllers.URLShortenerController
 
-// InjectURLShortenerRoutes is defined to set up the routes and inject the dependencies for the router to work correctly.
+// InjectURLShortenerRoutes sets up the necessary routes for the URL shortener service
+// and injects the required dependencies to the controller.
 func InjectURLShortenerRoutes(router *gin.Engine) {
 	setupRoutes(router)
 }
 
-// setupRoutes is defined to set up the router's endpoints.
+// setupRoutes configures the individual API endpoints for the URL shortener service.
 func setupRoutes(r *gin.Engine) {
 
-	// create a new instance of the container
+	// Initialize the container and inject the URLShortenerController
 	container := containers.NewContainer()
 	urlShortenerController = container.InjectURLShortenerController()
 
-	// a GET request to /order/:id will fetch an order
+	// Route to resolve a short URL and redirect to the original long URL
+	// GET /:short_url
 	r.GET("/:short_url", func(c *gin.Context) {
 		urlShortenerController.ResolveURL(c)
 	})
 
+	// Versioned routes under /v1
 	v1 := r.Group("/v1")
 
+	// Route to fetch all short URLs and their associated data
+	// GET /v1/urls
 	v1.GET("/urls", func(c *gin.Context) {
 		urlShortenerController.GetAllShortURLs(c)
 	})
 
-	// a POST request to /order will create an order
+	// Route to shorten a long URL (optionally with a custom short URL)
+	// POST /v1/shorten
 	v1.POST("/shorten", func(c *gin.Context) {
 		urlShortenerController.ShortenURL(c)
 	})
